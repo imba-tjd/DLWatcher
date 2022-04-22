cd81a0df62e3eb914d0382e2072e5da7dd17c407
imba-tjd <109224573@qq.com>
2022-04-22 13:37:31 +0800

    run in actions.yml rather than deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,7 @@
 set -ex
 
 case "$(git branch --show-current)" in master|main) echo "Running on a wrong branch." && exit 1; esac
-
-python3 dlwatcher.py
-mv data.html index.html
-rm data_tmpl.html deploy.sh dlwatcher.py
-
-if [ -z "$(git status -s)" ]; then exit; fi
+if [ -z "$(git status -s -uno)" ]; then exit; fi
 
 git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config --local user.name  "GitHub Actions"
